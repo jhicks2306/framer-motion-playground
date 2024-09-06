ae7320419d64ee6af93af1f52a24daf42afc1515
jhicks2306 <jhicks2306@gmail.com>
2024-09-06 21:46:06 +0100

    Testing keyframes

--- app/mycomponent.tsx
+++ app/mycomponent.tsx
@@ -5,11 +5,11 @@ export const MyComponent = () => (
   <motion.div
     className="w-24 h-24 bg-red-500"
     initial={{ opacity: 0, scale: 0.5 }}
-    animate={{ opacity: 1, scale: 1 }}
+    animate={{ opacity: 1, scale: [1, 1.5, 1, 2, 1]}}
     whileHover={{
       scale: 1.2,
       transition: { duration: 0.1}
     }}
-    transition={{ duration: 0.5 }}
+    transition={{ duration: 2 }}
   />
 )
